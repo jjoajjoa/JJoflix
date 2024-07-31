b859fa6046cdfb33fa3f8124c0e71fe9b43f3713
Arum <rlfehd429@gmail.com>
2024-08-01 00:59:51 +0900

    Add

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const express = require("express");
+const path = require('path');
+
+// const mariadb = require('mariadb');
+// const pool = mariadb.createPool({
+//     host: 'localhost',
+//     port: 4406,
+//     user: 'root',
+//     password: 'admin'
+// })
+
+BigInt.prototype.toJSON = () => {
+    return this.toString();
+}
+
+const app = express();
+const server = http.createServer(app);
+
+// 정적 파일 제공 설정
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname)); // index.html 및 다른 정적 파일 제공
+
+// 루트 경로에서 index.html 파일 제공
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = 7001;
+server.listen(port, () => {
+    console.log(`server is listening at localhost:${port}`);
+});
+console.log(`웹서버 실행 요청함`);
